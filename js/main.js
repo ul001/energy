fa9ff91e0f73a4fdd982f80e537e6f9c8a43ba71
panhong <381500590@qq.com>
2020-05-25 09:46:33 +0800

    合并代码

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-bui.ready(function() {
+bui.ready(function () {
     var storage = bui.storage();
     // 右边出来对话框
     var uiDialogRight = bui.dialog({
@@ -9,24 +9,32 @@ bui.ready(function() {
         buttons: []
     });
 
-    try{
+    try {
         var saveBuild = JSON.parse(storage.get("build"));
         $(".bui-bar-main").html(saveBuild.name);
-    }catch(e){
+    } catch (e) {
         uiDialogRight.open();
     };
 
-    $('#btnOpenFilter').on("click", function() {
+    $('#BackToAPP').on("click", function () {
+        if (isIOS) {
+            window.webkit.messageHandlers.goBackiOS.postMessage("");
+        } else {
+            android.goBack();
+        }
+    });
+
+    $('#btnOpenFilter').on("click", function () {
         uiDialogRight.open();
     });
 
     var uiSearchbar = bui.searchbar({
         id: "#searchbar",
-        onInput: function(e, keyword) {
+        onInput: function (e, keyword) {
             //实时搜索
             // console.log(++n)
         },
-        onRemove: function(e, keyword) {
+        onRemove: function (e, keyword) {
             uiList.empty();
             // 重新初始化数据
             uiList.init({
@@ -36,7 +44,7 @@ bui.ready(function() {
                 }
             });
         },
-        callback: function(e, keyword) {
+        callback: function (e, keyword) {
             if (uiList) {
                 //点击搜索清空数据
                 uiList.empty();
@@ -54,7 +62,9 @@ bui.ready(function() {
     var uiList = bui.list({
         id: "#scrollList",
         url: "/api/app/AppBuild",
-        headers: {"Authorization":tokenFromAPP},
+        headers: {
+            "Authorization": tokenFromAPP
+        },
         pageSize: 15, // 当pageSize 小于返回的数据大小的时候,则认为是最后一页,接口返回的数据最好能返回空数组,而不是null
         data: {},
         //如果分页的字段名不一样,通过field重新定义
@@ -63,19 +73,22 @@ bui.ready(function() {
             size: "pageSize",
             data: "data"
         },
-        callback: function(e) {
+        callback: function (e) {
             // e.target 为你当前点击的元素
             // e.currentTarget 为你当前点击的handle 整行
             var buildId = $(e.currentTarget).attr("data-id");
             var buildName = $(e.currentTarget).attr("data-name");
-            var clickBuild = {id:buildId,name:buildName};
+            var clickBuild = {
+                id: buildId,
+                name: buildName
+            };
             storage.set("build", JSON.stringify(clickBuild));
             $(".bui-bar-main").html(buildName);
             uiDialogRight.close();
         },
-        template: function(data) {
+        template: function (data) {
             var html = "";
-            data.forEach(function(el, index) {
+            data.forEach(function (el, index) {
                 html += `<li class="bui-btn bui-box" data-id="${el.id}" data-name="${el.name}">
                              <div class="icon"><i class="icon-sub"></i></div>
                              <div class="span1">${el.name}</div>
@@ -83,20 +96,23 @@ bui.ready(function() {
             });
             return html;
         },
-        onBeforeRefresh: function() {
+        onBeforeRefresh: function () {
             console.log("brefore refresh")
         },
-        onBeforeLoad: function() {
+        onBeforeLoad: function () {
             console.log("brefore load")
         },
-        onRefresh: function() {
+        onRefresh: function () {
             // 刷新以后执行
             console.log("refreshed")
         },
-        onLoad: function() {
+        onLoad: function () {
             // 刷新以后执行
             console.log("loaded")
         }
     });
     return uiList;
+
+
+
 });
